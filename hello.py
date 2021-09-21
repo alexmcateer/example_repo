d597377ce11eed6f4255b21d4603ec499408855b
Alex McAteer <alex.mcateer14@gmail.com>
2021-09-21 15:50:34 +0100

    this is an update

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
+def Method():
+    print("This is a second method call!")
+
 def Hello():
     print("Hey there! This is a method, it is pretty useless if you ask me!")
+    Method()
 
 if __name__ == '__main__':
     Hello()

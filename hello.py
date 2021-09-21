9a5ee8daa133cde070f63495b172177e5f5cc102
Alex McAteer <alex.mcateer14@gmail.com>
2021-09-21 15:51:55 +0100

    I dont think this will work

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def Hello():
-    print("Hey there! This is a method, it is pretty useless if you ask me!")
+    print("Hey there! This is a method, it is not useless if you ask me!")
 
 if __name__ == '__main__':
     Hello()
